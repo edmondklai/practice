f16609013b406f3d3fbf00cd7098653f07540278
Edmond <edmondklai@gmail.com>
2023-07-01 13:32:46 -0500

    practice question 1-3

---
+++ week3.js
@@ -0,0 +1,13 @@
+// Question 1
+const name = "Edmond"
+console.log(name)
+
+// Question 2
+let length = 10
+console.log(length + 15)
+
+// Question 3
+const studentId = '1234'
+const studentName = "Edmond"
+const schoolName = "Chicago School"
+console.log("My name is " + studentName + ". My student ID is" + studentId + "and I go to " + schoolName)
