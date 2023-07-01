50c9584afc91bde02589690bd700532f6e0f0e28
Edmond <edmondklai@gmail.com>
2023-07-01 13:53:17 -0500

    add area function

--- week3.js
+++ week3.js
@@ -11,3 +11,18 @@ const studentId = '1234'
 const studentName = "Edmond"
 const schoolName = "Chicago School"
 console.log("My name is " + studentName + ". My student ID is" + studentId + "and I go to " + schoolName)
+
+
+// ! Function should have 1 input, radius
+// ? Function should have 1 output, area. Or you can print the area given the radius
+function area(radius) {
+  return 3.14 * radius * radius
+}
+
+const areaAnswer = area(15)
+console.log(areaAnswer)
+
+
+// ! git add week3.js
+// ! git commit -m "add area function"
+// ! git push
